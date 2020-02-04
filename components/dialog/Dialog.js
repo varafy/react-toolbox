1196910d96dec63ae262c09f4d307f494e5c9634
Justin Schultz <jaschultz25@gmail.com>
2020-02-04 11:04:18 -0700

    Adds ButtonComponent acceptance from Dialog.

--- components/dialog/Dialog.js
+++ components/dialog/Dialog.js
@@ -11,9 +11,11 @@ import InjectOverlay from '../overlay/Overlay';
 
 const factory = (Overlay, Button) => {
   const Dialog = (props) => {
+    const ButtonComponent = props.ButtonComponent || Button;
+
     const actions = props.actions.map((action, idx) => {
       const className = classnames(props.theme.button, { [action.className]: action.className });
-      return <Button key={idx} {...action} className={className} />; // eslint-disable-line
+      return <ButtonComponent key={idx} {...action} className={className} />; // eslint-disable-line
     });
 
     const className = classnames([props.theme.dialog, props.theme[props.type]], {
@@ -58,6 +60,7 @@ const factory = (Overlay, Button) => {
       label: PropTypes.string,
     })),
     active: PropTypes.bool,
+    ButtonComponent: PropTypes.func,
     children: PropTypes.node,
     className: PropTypes.string,
     onEscKeyDown: PropTypes.func,
